e73ecde1e76b46cb56f5b0c5ac476ddc09b329d9
Alexey Sokolov <alexey@ebi.ac.uk>
2022-11-16 10:43:39 +0000

    Updating BE

---
+++ app/main.py
@@ -0,0 +1,172 @@
+import os
+
+from elasticsearch import AsyncElasticsearch, AIOHttpConnection
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import json
+
+from .constants import DATA_PORTAL_AGGREGATIONS
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+ES_HOST = os.getenv('ES_HOST')
+ES_USERNAME = os.getenv('ES_USERNAME')
+ES_PASSWORD = os.getenv('ES_PASSWORD')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+es = AsyncElasticsearch(
+    [ES_HOST], connection_class=AIOHttpConnection,
+    http_auth=(ES_USERNAME, ES_PASSWORD),
+    use_ssl=True, verify_certs=False)
+
+
+@app.get("/summary")
+async def summary():
+    response = await es.search(index="summary")
+    data = dict()
+    data['results'] = response['hits']['hits']
+    return data
+
+
+@app.get("/{index}")
+async def root(index: str, offset: int = 0, limit: int = 15,
+               sort: str = "rank:desc", filter: str | None = None,
+               search: str | None = None, current_class: str = 'kingdom',
+               phylogeny_filters: str | None = None):
+    print(phylogeny_filters)
+    # data structure for ES query
+    body = dict()
+    # building aggregations for every request
+    body["aggs"] = dict()
+    for aggregation_field in DATA_PORTAL_AGGREGATIONS:
+        body["aggs"][aggregation_field] = {
+            "terms": {"field": aggregation_field}
+        }
+    body["aggs"]["taxonomies"] = {
+        "nested": {"path": f"taxonomies.{current_class}"},
+        "aggs": {current_class: {
+            "terms": {
+                "field": f"taxonomies.{current_class}.scientificName"
+            }
+        }
+        }
+    }
+
+    if phylogeny_filters:
+        body["query"] = {
+            "bool": {
+                "filter": list()
+            }
+        }
+        phylogeny_filters = phylogeny_filters.split("-")
+        print(phylogeny_filters)
+        for phylogeny_filter in phylogeny_filters:
+            name, value = phylogeny_filter.split(":")
+            nested_dict = {
+                "nested": {
+                    "path": f"taxonomies.{name}",
+                    "query": {
+                        "bool": {
+                            "filter": list()
+                        }
+                    }
+                }
+            }
+            nested_dict["nested"]["query"]["bool"]["filter"].append(
+                {
+                    "term": {
+                        f"taxonomies.{name}.scientificName": value
+                    }
+                }
+            )
+            body["query"]["bool"]["filter"].append(nested_dict)
+    # adding filters, format: filter_name1:filter_value1, etc...
+    if filter:
+        filters = filter.split(",")
+        if 'query' not in body:
+            body["query"] = {
+                "bool": {
+                    "filter": list()
+                }
+            }
+        for filter_item in filters:
+            if current_class in filter_item:
+                _, value = filter_item.split(":")
+                nested_dict = {
+                    "nested": {
+                        "path": f"taxonomies.{current_class}",
+                        "query": {
+                            "bool": {
+                                "filter": list()
+                            }
+                        }
+                    }
+                }
+                nested_dict["nested"]["query"]["bool"]["filter"].append(
+                    {
+                        "term": {
+                            f"taxonomies.{current_class}.scientificName": value
+                        }
+                    }
+                )
+                body["query"]["bool"]["filter"].append(nested_dict)
+            else:
+                filter_name, filter_value = filter_item.split(":")
+                body["query"]["bool"]["filter"].append(
+                    {"term": {filter_name: filter_value}}
+                )
+
+    # adding search string
+    if search:
+        # body already has filter parameters
+        if "query" in body:
+            # body["query"]["bool"].update({"should": []})
+            body["query"]["bool"].update({"must": {}})
+        else:
+            # body["query"] = {"bool": {"should": []}}
+            body["query"] = {"bool": {"must": {}}}
+        body["query"]["bool"]["must"] = {"bool": {"should": []}}
+        body["query"]["bool"]["must"]["bool"]["should"].append(
+            {"wildcard": {"organism": {"value": f"*{search}*",
+                                       "case_insensitive": True}}}
+        )
+        body["query"]["bool"]["must"]["bool"]["should"].append(
+            {"wildcard": {"commonName": {"value": f"*{search}*",
+                                         "case_insensitive": True}}}
+        )
+    print(json.dumps(body))
+    response = await es.search(
+        index=index, sort=sort, from_=offset, size=limit, body=body
+    )
+    data = dict()
+    data['count'] = response['hits']['total']['value']
+    data['results'] = response['hits']['hits']
+    data['aggregations'] = response['aggregations']
+    return data
+
+
+@app.get("/{index}/{record_id}")
+async def details(index: str, record_id: str):
+    body = dict()
+    if index == 'data_portal':
+        body["query"] = {
+            "bool": {"filter": [{'term': {'organism': record_id}}]}}
+        response = await es.search(index=index, body=body)
+    else:
+        response = await es.search(index=index, q=f"_id:{record_id}")
+    data = dict()
+    data['count'] = response['hits']['total']['value']
+    data['results'] = response['hits']['hits']
+    return data
