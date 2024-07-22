4c5baa1511bdbb0ad654b6b46b73b8d024cfa948
raheela <32760007+Raheela1024@users.noreply.github.com>
2024-07-22 14:26:42 +0100

    api added for downloader utility.

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import json
 
 from .constants import DATA_PORTAL_AGGREGATIONS
 
-app = FastAPI(openapi_url="/openapi.json")
+app = FastAPI()
 
 origins = [
     "*"
@@ -17,6 +17,7 @@ origins = [
 ES_HOST = os.getenv('ES_HOST')
 ES_USERNAME = os.getenv('ES_USERNAME')
 ES_PASSWORD = os.getenv('ES_PASSWORD')
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,6 +31,123 @@ es = AsyncElasticsearch(
     [ES_HOST], connection_class=AIOHttpConnection,
     http_auth=(ES_USERNAME, ES_PASSWORD),
     use_ssl=True, verify_certs=False)
+
+
+@app.get("/downloader_utility_data/")
+async def downloader_utility_data(taxonomy_filter: str, data_status: str, experiment_type: str, project_name: str):
+    body = dict()
+    if taxonomy_filter != '':
+
+        if taxonomy_filter:
+            body["query"] = {
+                "bool": {
+                    "filter": list()
+                }
+            }
+            nested_dict = {
+                "nested": {
+                    "path": "taxonomies.class",
+                    "query": {
+                        "bool": {
+                            "filter": list()
+                        }
+                    }
+                }
+            }
+            nested_dict["nested"]["query"]["bool"]["filter"].append(
+                {
+                    "term": {
+                        "taxonomies.class.scientificName": taxonomy_filter
+                    }
+                }
+            )
+            body["query"]["bool"]["filter"].append(nested_dict)
+
+    if data_status is not None and data_status != '':
+        split_array = data_status.split("-")
+        if split_array and split_array[0].strip() == 'Biosamples':
+            body["query"]["bool"]["filter"].append(
+                {"term": {'biosamples': split_array[1].strip()}}
+            )
+        elif split_array and split_array[0].strip() == 'Raw Data':
+            body["query"]["bool"]["filter"].append(
+                {"term": {'raw_data': split_array[1].strip()}}
+            )
+        elif split_array and split_array[0].strip() == 'Mapped Reads':
+            body["query"]["bool"]["filter"].append(
+                {"term": {'mapped_reads': split_array[1].strip()}})
+
+        elif split_array and split_array[0].strip() == 'Assemblies':
+            body["query"]["bool"]["filter"].append(
+                {"term": {'assemblies_status': split_array[1].strip()}})
+        elif split_array and split_array[0].strip() == 'Annotation Complete':
+            body["query"]["bool"]["filter"].append(
+                {"term": {'annotation_complete': split_array[1].strip()}})
+        elif split_array and split_array[0].strip() == 'Annotation':
+            body["query"]["bool"]["filter"].append(
+                {"term": {'annotation_status': split_array[1].strip()}})
+        elif split_array and split_array[0].strip() == 'Genome Notes':
+            nested_dict = {
+                "nested": {
+                    "path": "genome_notes",
+                    "query": {
+                        "bool": {
+                            "must": [{
+                                "exists": {
+                                    "field": "genome_notes.url"
+                                }
+                            }]
+                        }
+                    }
+                }
+            }
+            body["query"]["bool"]["filter"].append(nested_dict)
+    if experiment_type != '' and experiment_type is not None:
+        nested_dict = {
+            "nested": {
+                "path": "experiment",
+                "query": {
+                    "bool": {
+                        "must": [{
+                            "term": {
+                                "experiment.library_construction_protocol.keyword": experiment_type
+                            }
+                        }]
+                    }
+                }
+            }
+        }
+        body["query"]["bool"]["filter"].append(nested_dict)
+    if project_name is not None and project_name != '':
+        body["query"]["bool"]["filter"].append(
+            {"term": {'project_name': project_name}})
+    print(body)
+    response = await es.search(index="data_portal", from_=0, size=10000, body=body)
+    total_count = response['hits']['total']['value']
+    result = response['hits']['hits']
+    results_count = len(response['hits']['hits'])
+    while total_count > results_count:
+        response1 = await es.search(index="data_portal", from_=results_count, size=10000, body=body)
+        result.extend(response1['hits']['hits'])
+        results_count += len(response1['hits']['hits'])
+
+    return result
+
+
+@app.get("/downloader_utility_data_with_species/")
+async def downloader_utility_data_with_species(species_list: str, project_name: str):
+    body = dict()
+    result = []
+    if species_list != '' and species_list is not None:
+        species_list_array = species_list.split(",")
+        for organism in species_list_array:
+            body["query"] = {
+                "bool": {"filter": [{'term': {'_id': organism}}, {'term': {'project_name': project_name}}]}}
+            response = await es.search(index='data_portal',
+                                       body=body)
+            result.extend(response['hits']['hits'])
+
+    return result
 
 
 @app.get("/summary")
