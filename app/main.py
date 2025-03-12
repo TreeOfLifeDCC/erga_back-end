98f3bc22e07fd4a22ece72f7f06875ba3fb3de7d
raheela <32760007+Raheela1024@users.noreply.github.com>
2025-03-12 15:28:56 +0000

    Download data in batches.

--- app/main.py
+++ app/main.py
@@ -181,32 +181,23 @@ class QueryParam(BaseModel):
     sortValue: str
     filterValue: str = ''
     currentClass: str
-    phylogenyFilters: str
-    indexName: str
+    phylogeny_filters: str
+    index_name: str
     downloadOption: str
 
 
 @app.post("/data-download")
 async def get_data_files(item: QueryParam):
-    data = await root(item.indexName, 0, item.pageSize,
-                      item.sortValue, item.filterValue,
-                      item.searchValue, item.currentClass,
-                      item.phylogenyFilters, 'download')
+    data = await fetch_data_in_batches(item)
+    csv_data = create_data_files_csv(data, item.downloadOption,
+                                         item.index_name)
 
-    if 'results' in data:
-        csv_data = create_data_files_csv(data['results'], item.downloadOption,
-                                         item.indexName)
+    return StreamingResponse(
+        csv_data,
+        media_type='text/csv',
+        headers={"Content-Disposition": "attachment; filename=download.csv"}
+    )
 
-        return StreamingResponse(
-            csv_data,
-            media_type='text/csv',
-            headers={"Content-Disposition": "attachment; filename=download.csv"}
-        )
-    else:
-        return JSONResponse(
-            status_code=500,
-            content={"error": "There was an issue downloading the file"}
-        )
 
 
 def create_data_files_csv(results, download_option, index_name):
@@ -224,7 +215,7 @@ def create_data_files_csv(results, download_option, index_name):
     output = io.StringIO()
     csv_writer = csv.writer(output)
     csv_writer.writerow(header)
-
+    print(len(results))
     for entry in results:
         record = entry["_source"]
         if download_option.lower() == "metadata" and index_name in ['data_portal', 'data_portal_test']:
@@ -446,7 +437,7 @@ async def root(index: str, offset: int = 0, limit: int = 15,
     if action == 'download':
         try:
             response = await es.search(index=index, sort=sort, from_=offset,
-                                       body=body, size=10000)
+                                       body=body, size=limit)
         except ConnectionTimeout:
             return {"error": "Request to Elasticsearch timed out."}
     else:
@@ -472,3 +463,30 @@ async def details(index: str, record_id: str):
     data['count'] = response['hits']['total']['value']
     data['results'] = response['hits']['hits']
     return data
+
+
+
+async def fetch_data_in_batches(item: QueryParam):
+    offset = 0
+    batch_size = 1000
+    all_data = []
+
+    while True:
+
+        data = await root(
+            item.index_name, offset, batch_size,
+            item.sortValue, item.filterValue,
+            item.searchValue, item.currentClass,
+            item.phylogeny_filters, 'download'
+        )
+
+
+        results = data.get('results', [])
+        if not results:
+            break
+
+        all_data.extend(results)
+        offset += batch_size
+        print(f"Fetched {len(results)} results, total: {len(all_data)}")
+
+    return all_data
